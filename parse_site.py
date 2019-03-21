faaae7cdbb03562689a5077b7a60b6d38e1958e7
Zsuzsanna Horvath <zsuzsi@localhost.localdomain>
2019-03-21 19:53:52 +0100

    Stripped text from html stuff

--- parse_site.py
+++ parse_site.py
@@ -1,18 +1,23 @@
 import urllib.request
 from inscriptis import get_text
 import re
+import os
 
 #Add url to the site to be crawled and file path to write into
 url = "https://en.wikipedia.org/wiki/The_Marriage_of_Figaro_(play)"
-filePath = "/home/zsuzsi/study/python/web_page.txt"
+filePath = "/home/zsuzsi/study/python/chatbot/web_page.txt"
 
-#Get the site noted by the URL
+#Get the site requested by the URL
 response = urllib.request.urlopen(url)
-content = response.read()
+content = response.read().decode("utf-8")
+#Get only the text from the requested site
+text = get_text(str(content))
+#Get rid of unnecessary line breaks
+# textNobreak = re.sub("\n", "", text)
 
 #Write site content into file
-with open("/home/zsuzsi/study/python/web_page.txt", "w") as file:
-    file.write(str(content))
+with open(filePath, "w") as file:
+    file.write(str(text))
 
 file.close()
 
