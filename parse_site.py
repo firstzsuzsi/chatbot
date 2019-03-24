775f2686e41bb5d1517c05631de4c0e2423a1409
Zsuzsanna Horvath <zsuzsi@localhost.localdomain>
2019-03-24 14:56:45 +0100

    Split the word frecuency analysis into a different code

--- parse_site.py
+++ parse_site.py
@@ -2,11 +2,6 @@ import urllib.request
 from inscriptis import get_text
 import os
 
-
-# Add url to the site to be crawled and file path to write into
-# url = "https://en.wikipedia.org/wiki/The_Marriage_of_Figaro_(play)"
-# filePath = "/home/zsuzsi/study/python/chatbot/web_page.txt"
-
 def parse(url, filePath):
     # Get the site requested by the URL
     response = urllib.request.urlopen(url)
@@ -23,26 +18,3 @@ def parse(url, filePath):
 urlTest = "https://en.wikipedia.org/wiki/The_Marriage_of_Figaro_(play)"
 filePathTest = "/home/zsuzsi/study/python/chatbot/web_page.txt"
 parse(urlTest, filePathTest)
-
-# This part is to go into the word_frequency.py file, once I have figured out how to run the parser function in there.
-
-# Get text from a website written in a file
-url = "https://en.wikipedia.org/wiki/The_Marriage_of_Figaro_(play)"
-filePath = "/home/zsuzsi/study/python/chatbot/web_page.txt"
-parse(url, filePath)
-
-# Store the frequency of words in a dictionary
-frequency = {}
-
-# Open the file with the website content
-filePath = "/home/zsuzsi/study/python/chatbot/web_page.txt"
-with open(filePath, "r") as text:
-    words = text.read()
-    # Tokenize the text
-    for tokens in words.split():
-        # Count the frequency of the tokens
-        if tokens in frequency:
-            frequency[tokens] = frequency[tokens] + 1
-        else:
-            frequency[tokens] = 1
-print(frequency)
