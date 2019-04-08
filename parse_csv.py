181eb0a2fa21af4804fb80a80be7371289ed5fd6
Zsuzsi <zsuzsivagyokenis@gmail.com>
2019-04-08 15:44:32 +0200

    Reorganized the project

--- parse_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-filePath = ("/home/zsuzsi/study/python/chatbot/crime_stats.csv")
-with open(filePath, "rt") as stats:
-    read = csv.reader(stats, delimiter = ",", quotechar = "'")
-    for line in read:
-        print(line)
